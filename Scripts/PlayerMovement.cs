e3bcf4510406708f818ee9ef49a19fbf180ae31a
Ahmad Diab <133016705+AhmadDiab0@users.noreply.github.com>
2025-06-27 16:29:14 +0200

    added wall jumping to the movement

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -1,25 +1,30 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+
     [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
     private Rigidbody2D body;
     private Animator anim;
-    private bool grounded;
-
+    private BoxCollider2D boxCollider;
+    private float wallJumpCooldown;
+    private float horizontalInput;
     private void Awake()
     {
         // Refrence the Rigidbody2D and Animator components
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocityY);
+        horizontalInput = Input.GetAxis("Horizontal");
 
         // Flip the player sprite based on horizontal input
         if (horizontalInput > 0.01f)
@@ -31,35 +36,67 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(-3, 3, 3);
         }
 
-        // Check for jump input
-        // and if the player is grounded
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
-        {
-            jump();
-        }
-
         // set the animator parameters
         anim.SetBool("run", horizontalInput != 0);
-        anim.SetBool("grounded", grounded);
+        anim.SetBool("grounded", isGrounded());
+
+        // Wall jump logic
+        if (wallJumpCooldown > 0.2f)
+        {
+            body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocityY);
+
+            if (onWall() && !isGrounded())
+            {
+                body.gravityScale = 0;
+                body.linearVelocity = Vector2.zero;
+            }
+            else
+                body.gravityScale = 3;
+            // Check for jump input
+            if (Input.GetKeyDown(KeyCode.Space))
+                jump();
+
+        }
+        else
+            wallJumpCooldown += Time.deltaTime;
     }
 
     private void jump()
     {
-        if (grounded)
+        if (isGrounded())
         {
-            body.linearVelocity = new Vector2(body.linearVelocityX, speed);
+            body.linearVelocity = new Vector2(body.linearVelocityX, jumpForce);
             anim.SetTrigger("jump");
-            // Set grounded to false after jumping
-            grounded = false;
+        }
+        else if (onWall() && !isGrounded())
+        {
+            if (horizontalInput == 0)
+            {
+                body.linearVelocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+
+            }
+            else
+                body.linearVelocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+
+            wallJumpCooldown = 0;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Check if the player is grounded by checking if it collides with the ground layer
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            grounded = true;
-        }
+
+    }
+    private bool isGrounded()
+    {
+        // Check if the player is grounded by casting a ray downwards
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
     }
 }
